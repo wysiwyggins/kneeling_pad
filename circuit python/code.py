1c7e63d630c79d22da56aca81194a4fc7ac8cf72
Wiley Wiggins <weevil@wileywiggins.com>
2023-02-09 16:34:18 -0800

    attempting a mqtt version

--- circuit python/code.py
+++ circuit python/code.py
@@ -1,118 +1,56 @@
+# SPDX-FileCopyrightText: 2019 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
 import time
 import board
+import busio
 import digitalio
-import usb_hid
+import adafruit_esp32spi.adafruit_esp32spi_socket as socket
+from adafruit_esp32spi import adafruit_esp32spi
+import adafruit_minimqtt.adafruit_minimqtt as MQTT
 
-from adafruit_hid.keyboard import Keyboard
-from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
-from adafruit_hid.keycode import Keycode
+# Get wifi details and more from a secrets.py file
+try:
+    from secrets import secrets
+except ImportError:
+    print("WiFi secrets are kept in secrets.py, please add them there!")
+    raise
 
-from adafruit_debouncer import Debouncer
 
-# from adafruit_ble import BLERadio
-# from adafruit_ble.advertising.standard import ProvideServicesAdvertisement
-# from adafruit_ble.services.nordic import UARTService
+# If you are using a board with pre-defined ESP32 Pins:
+esp32_cs = digitalio.DigitalInOut(board.D10)
+esp32_ready = digitalio.DigitalInOut(board.D9)
+esp32_reset = digitalio.DigitalInOut(board.D5)
 
-# from adafruit_airlift.esp32 import ESP32
 
-# esp32 = ESP32(
-#     reset=board.D12,
-#     gpio0=board.D10,
-#     busy=board.D11,
-#     chip_select=board.D13,
-#     tx=board.TX,
-#     rx=board.RX,
-# )
+spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
+esp = adafruit_esp32spi.ESP_SPIcontrol(spi, esp32_cs, esp32_ready, esp32_reset)
 
-# adapter = esp32.start_bluetooth()
+mqtt = MQTT.MQTT(esp, secrets["broker"], "1883")
 
-# ble = BLERadio(adapter)
-# uart = UARTService()
-# advertisement = ProvideServicesAdvertisement(uart)
+def message_received(topic, msg):
+    print("Received message from topic '{}': {}".format(topic, msg))
 
-# while True:
-#     ble.start_advertising(advertisement)
-#     print("waiting to connect")
-#     while not ble.connected:
-#         pass
-#     print("connected: trying to read input")
-#     while ble.connected:
-#         # Returns b'' if nothing was read.
-#         one_byte = uart.read(1)
-#         if one_byte:
-#             print(one_byte)
-#             uart.write(one_byte)
 
-def createButton(pinRef):
-  btn = digitalio.DigitalInOut(pinRef)
-  btn.direction = digitalio.Direction.INPUT
-  btn.pull = digitalio.Pull.UP
-  return btn
+# Subscribe to the API at mudroom.rip using the security token
+security_token = secrets["token"]
+mqtt.subscribe("JSON_URL" + security_token, message_received)
 
-#button_fire = createButton(board.A0)
-button_up = createButton(board.A3)
-button_down = createButton(board.D24)
-button_left = createButton(board.A1)
-button_right = createButton(board.A2)
+# Define a button and a flag to keep track of its state
+button = digitalio.DigitalInOut(board.D7)
+button.direction = digitalio.Direction.INPUT
+button.pull = digitalio.Pull.UP
 
-pin_fire = digitalio.DigitalInOut(board.A0)
-pin_fire.direction = digitalio.Direction.INPUT
-pin_fire.pull = digitalio.Pull.UP
-button_fire = Debouncer(pin_fire)
+button_state = False
 
-pin_kneel = digitalio.DigitalInOut(board.D25)
-pin_kneel.direction = digitalio.Direction.INPUT
-pin_kneel.pull = digitalio.Pull.UP
-button_kneel = Debouncer(pin_kneel)
+# Define the callback function for the button
+def button_callback(change):
+    global button_state
+    button_state = not button_state
+    mqtt.publish("mudroom.rip/api/v1/game/rooms/kneel/", str(button_state), qos=0)
 
-led = digitalio.DigitalInOut(board.LED)
-led.direction = digitalio.Direction.OUTPUT
-
-time.sleep(1)
-keyboard = Keyboard(usb_hid.devices)
-keyboard_layout = KeyboardLayoutUS(keyboard)
+# Attach the button callback function to the button
+button.callback = button_callback
 
 while True:
-    button_kneel.update()
-    button_fire.update()
-
-    if button_fire.fell:
-        led.value = True
-        keyboard.press(Keycode.ENTER)
-    elif button_fire.rose:
-        led.value = False
-        keyboard.release(Keycode.ENTER)
-
-    if not button_up.value:
-        led.value = True
-        keyboard.press(Keycode.UP_ARROW)
-    else:
-        keyboard.release(Keycode.UP_ARROW)
-    
-    if not button_down.value:
-        led.value = True
-        keyboard.press(Keycode.DOWN_ARROW)
-    else:
-        keyboard.release(Keycode.DOWN_ARROW)
-
-    if not button_left.value:
-        led.value = True
-        keyboard.press(Keycode.LEFT_ARROW)
-    else:
-        keyboard.release(Keycode.LEFT_ARROW)
-
-    if not button_right.value:
-        led.value = True
-        keyboard.press(Keycode.RIGHT_ARROW)
-    else:
-        led.value = False
-        keyboard.release(Keycode.RIGHT_ARROW)
-
-    if button_kneel.fell:
-        led.value = True
-        keyboard_layout.press("p")
-    elif button_kneel.rose:
-        led.value = False
-        keyboard_layout.release("p")
-
-    time.sleep(0.01)
+    mqtt.loop()
