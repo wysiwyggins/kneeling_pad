76a8c08f1bbeba10115c7f35b2e660bc19828ad7
Wiley Wiggins <weevil@wileywiggins.com>
2023-03-06 16:49:46 -0800

    thrashing, mqtt connect is unhappy about a non-int port so connect is probably malformed

--- circuit python/code.py
+++ circuit python/code.py
@@ -1,67 +1,85 @@
-
 import board
+import digitalio
 import busio
 import digitalio
-import time
-from adafruit_esp32spi import adafruit_esp32spi_socket as socket
+import neopixel
+import adafruit_debouncer
 import adafruit_minimqtt.adafruit_minimqtt as MQTT
+import time
+from digitalio import DigitalInOut
+import adafruit_requests as requests
+import adafruit_esp32spi.adafruit_esp32spi_socket as socket
+from adafruit_esp32spi import adafruit_esp32spi
 
-# Get wifi details and more from a secrets.py file
 try:
     from secrets import secrets
 except ImportError:
     print("WiFi secrets are kept in secrets.py, please add them there!")
     raise
 
-# Set up ESP32 SPI connection
-esp32_cs = digitalio.DigitalInOut(board.D9)
-esp32_ready = digitalio.DigitalInOut(board.D10)
-esp32_reset = digitalio.DigitalInOut(board.D5)
+
+# Define digital input for button
+button_pin = digitalio.DigitalInOut(board.D7)
+button_pin.direction = digitalio.Direction.INPUT
+button_pin.pull = digitalio.Pull.UP
+button = adafruit_debouncer.Debouncer(button_pin)
+
+# Define neopixel to indicate button presses
+pixels = neopixel.NeoPixel(board.NEOPIXEL, 1, brightness=0.2)
+
+# If you are using a board with pre-defined ESP32 Pins:
+esp32_cs = DigitalInOut(board.ESP_CS)
+esp32_ready = DigitalInOut(board.ESP_BUSY)
+esp32_reset = DigitalInOut(board.ESP_RESET)
+
+
 spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
 esp = adafruit_esp32spi.ESP_SPIcontrol(spi, esp32_cs, esp32_ready, esp32_reset)
+mqtt = MQTT.MQTT(broker=secrets["broker"], port=secrets["port"])
 
-# Connect to WiFi
-print("Connecting to WiFi...")
+
+def message_received(topic, msg):
+    print("Received message from topic '{}': {}".format(topic, msg))
+
+
+# Define function to handle button presses
+def handle_button_press(pin):
+    if not button.value:
+        # Turn on neopixel
+        pixels.fill((255, 0, 0))
+        # Publish MQTT message
+        mqtt.publish("kneels/get", "Knelt in devotion")
+        # Wait a bit to debounce
+        time.sleep(0.1)
+        # Turn off neopixel
+        pixels.fill((0, 0, 0))
+
+
+# Attach button interrupt
+button.when_pressed = handle_button_press
+
+if esp.status == adafruit_esp32spi.WL_IDLE_STATUS:
+    print("ESP32 found and in idle mode")
+print("Firmware vers.", esp.firmware_version)
+print("MAC addr:", [hex(i) for i in esp.MAC_address])
+
+for ap in esp.scan_networks():
+    print("\t%s\t\tRSSI: %d" % (str(ap["ssid"], "utf-8"), ap["rssi"]))
+
+print("Connecting to AP...")
 while not esp.is_connected:
     try:
-        esp.connect_AP("ssid", "password")
-    except RuntimeError as e:
-        print("Could not connect to WiFi, retrying...")
+        esp.connect_AP(secrets["ssid"], secrets["password"])
+    except OSError as e:
+        print("could not connect to AP, retrying: ", e)
         continue
-print("Connected to WiFi")
-
-# Set up MQTT client
-mqtt_broker = "192.168.50.93"
-mqtt_topic = "kneels/get"
-client_id = "circuitpython_client"
-mqtt_client = MQTT.MQTT(socket, broker=mqtt_broker, client_id=client_id)
-
-# Define button pin
-button = digitalio.DigitalInOut(board.D7)
-button.direction = digitalio.Direction.INPUT
-button.pull = digitalio.Pull.UP
-
-# Define function to publish message on button press
-def on_button_press(_):
-    message = "Knelt in devotion at {}".format(time.monotonic())
-    print("Publishing message: ", message)
-    mqtt_client.publish(mqtt_topic, message)
-
-# Set up button interrupt
-button.add_event_detect(digitalio.EdgeFalling, callback=on_button_press)
+print("Connected to", str(esp.ssid, "utf-8"), "\tRSSI:", esp.rssi)
+print("My IP address is", esp.pretty_ip(esp.ip_address))
 
 # Connect to MQTT broker
 print("Connecting to MQTT broker...")
-while not mqtt_client.is_connected:
-    try:
-        esp.start_ssl(mqtt_broker)
-        socket.set_interface(esp)
-        mqtt_client.connect()
-    except socket.gaierror as e:
-        print("Could not connect to MQTT broker, retrying...")
-        time.sleep(1)
+mqtt.connect()
 print("Connected to MQTT broker")
 
-while True:
-    mqtt_client.loop()
-    time.sleep(1)
+# Loop forever, handling MQTT messages and button presses
+while True
---
+++ circuit python/secrets.py
@@ -0,0 +1,16 @@
+# SPDX-FileCopyrightText: 2020 Limor Fried for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+
+# This file is where you keep secret settings, passwords, and tokens!
+# If you put them in the code you risk committing that info or sharing it
+
+secrets = {
+    # 'ssid' : 'MySpectrumWiFif8-2G',
+    # 'password' : 'plaincow150',
+     'ssid' : 'mudroom',
+     'password' : 'desmadungeon23',
+     'timezone' : "America/Chicago", # http://worldtimeapi.org/timezones
+     'token' : 'Token 09ffd5502a88adff798a5299455c530ee9db6a48',
+     'broker' : '192.168.50.93',
+    }
